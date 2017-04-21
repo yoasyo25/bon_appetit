aef8053a1075626674f8b4c3eec444b880aef325
Yohanan Assefa <nanahoy@gmail.com>
2017-04-21 08:16:28 -0600

    added add_to_cookbook test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -82,4 +82,27 @@ class PantryTest < Minitest::Test
     assert_equal "* Cheese: 25\n* Flour: 20\n* Noodles: 10\n* Marinara Sauce: 10", pantry.print_shopping_list
   end
 
+
+  def test_cookbook_takes_in_recipes
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry = Pantry.new
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    assert_equal [r1, r2, r3], pantry.cookbook
+  end
+
 end
